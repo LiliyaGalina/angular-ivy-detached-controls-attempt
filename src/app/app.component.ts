b51ee40596bb9d9f2b9c0d782d1979f169a19b2f
LiliyaGalina <33450441+LiliyaGalina@users.noreply.github.com>
2022-10-06 16:41:39 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,44 @@
 import { ChangeDetectionStrategy, Component, VERSION } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, UntypedFormArray } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  UntypedFormArray,
+} from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
   form: FormGroup;
+  resetCount = 0;
 
   get arrayItems(): AbstractControl[] {
     return (this.form.controls.array as UntypedFormArray).controls;
   }
 
-  constructor(
-    private formBuilder: FormBuilder
-  ) {
+  constructor(private formBuilder: FormBuilder) {
     this.form = this.formBuilder.group({
       array: this.formBuilder.array([]),
     });
-   this.reset();
+    this.reset();
   }
 
   public reset(): void {
+    this.resetCount += 1;
     const arr = this.form.get('array') as UntypedFormArray;
     arr.clear();
-    for (let i = 0; i < 1000; i++) {
-      arr.push(this.formBuilder.group({
-        someValue: [i]
-      }))
+    for (let i = 0; i < 10000; i++) {
+      arr.push(
+        this.formBuilder.group({
+          someValue: [this.resetCount * i + i],
+        })
+      );
     }
+    console.log('reset');
   }
 }
